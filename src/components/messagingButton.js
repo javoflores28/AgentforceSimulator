da7d23330d5b780c4a61a4c41143519d34446ba5
Javier Flores Zavala <javoflores28@hotmail.com>
2025-01-16 16:09:41 -0600

    Animation

--- src/components/messagingButton.js
+++ src/components/messagingButton.js
@@ -6,20 +6,24 @@ export default function MessagingButton(props) {
   const [isAnimating, setAnimating] = useState(false);
 
   const handleButtonClick = () => {
-    setAnimating(true); // Inicia la animación
+    // Inicia la animación
+    setAnimating(true);
 
     // Espera que termine la animación antes de continuar con el código adicional
     setTimeout(() => {
-      setAnimating(false); // Finaliza la animación
+      // Finaliza la animación
+      setAnimating(false);
+
+      // Asegúrate de llamar al manejador de clic proporcionado en las props
       if (props.clickHandler) {
         props.clickHandler(); // Ejecuta el manejador de clic después de la animación
       }
-    }, 4000); // Duración de la animación de 4 segundos
+    }, 6000); // Asegúrate de que el tiempo aquí coincida con la duración de tu animación
   }
 
   return (
     <>
-      <img src={robotImage} className={`robot-image ${isAnimating ? 'robot-animation' : ''}`} alt="Flying Robot" />
+      {isAnimating && <img src={robotImage} className="robot-animation" alt="Flying Robot" />}
       <button
         className="messagingButton"
         onClick={handleButtonClick}
@@ -31,6 +35,7 @@ export default function MessagingButton(props) {
     </>
   );
 }
+
 
 
 
